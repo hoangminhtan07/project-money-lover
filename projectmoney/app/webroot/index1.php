4a5249ea8b17489ca212f03d6655859139c67399
hoangminhtan07 <hayatebutler.07@gmail.com>
2015-06-23 17:53:11 +0700

    add project

---
+++ projectmoney/app/webroot/index1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA_Compatible" content="IE-edge">
+<meta name="viewport" content="width=device-width,intial-scale=1">
+<title>Money lover</title>
+<link rel="styleshet" href="css/bootstrap.min.css">
+<script src="js/jquery-1.11.3.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<body>
+<?php
+ echo "hi";
+?>
+</body>
+</html>
